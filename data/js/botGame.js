e9b020a545d5b8c97074b14348b8c740bf1f82c7
redtail <redtail5144>
2022-04-10 23:12:44 -0600

    Can now choose from 5 ai and play them

--- data/js/botGame.js
+++ data/js/botGame.js
@@ -17,12 +17,13 @@ var botClutch = [0.0, 0.0];
 // Initalizes everything
 // for the game
 function init() {
+  console.log("Init Start");
+  chooseBot();
   pScores = [];
   pTot = [0,0,0];
   botScores = [];
   botTot = [0,0,0];
   round = 0;
-  botSetup();
   getNames();
   document.getElementById("roundDisplay").innerHTML = "Round " + (round + 1);
 }
@@ -36,6 +37,67 @@ function getNames(){
     pName = prompt("Player 1 name:", "Player 1");
   } while (pName == null || pName == "")
   document.getElementById("name1").innerHTML = pName;
+  document.getElementById("name1.1").innerHTML = pName;
+}
+
+
+// Sets the bot up
+// with scoring probability
+function botSetup(name) {
+  var black, red, blue, cCall, cHit;
+
+  switch (name) {
+    case "Spenny":
+      black = 85.1;
+      red = 12.4;
+      blue = 0;
+      cCall = 93.9;
+      cHit = 40.3;
+      break;
+    case "Marksman":
+      black = 86.5;
+      red = 12.4;
+      blue = 0;
+      cCall = 24.3;
+      cHit = 31.5;
+      break;
+    case "Rander":
+      black = 86;
+      red = 9;
+      blue = 0.3;
+      cCall = 98;
+      cHit = 31.1;
+      break;
+    case "Battle Axe":
+      black = 79.7;
+      red = 18.8;
+      blue = 0;
+      cCall = 18.3;
+      cHit = 25.6;
+      break;
+    case "Coltron":
+      black = 77.2;
+      red = 21.2;
+      blue = 0;
+      cCall = 66.4;
+      cHit = 27;
+      break;
+  }
+
+  botWeights[0] = black; // Sets black
+  botWeights[1] = red; // Sets red
+  botWeights[2] = blue; // Sets blue
+  botWeights[3] = 100 - black - blue - red; // Sets miss
+  botClutch[0] = cCall; // Sets clutch call rate
+  botClutch[1] = cHit; // Sets clutch hit rate
+
+  console.log("Bot Stats: " + botWeights);
+
+  document.getElementById("name2").innerHTML = name;
+  document.getElementById("name2.2").innerHTML = name;
+
+  document.getElementById("game").style.display = "block";
+  document.getElementById("botStuff").style.display = "none";
 }
 
 // When target is clicked
@@ -70,7 +132,6 @@ function targetClick(value) {
 
     // Displays the throws
     let temp = (round * 5) + throwNum;
-    console.log("Temp: " + temp);
     document.getElementById('paxe'.concat(throwNum)).innerHTML = pScores[temp-1];
     document.getElementById('baxe'.concat(throwNum)).innerHTML = bScores[temp-1];
   }
@@ -86,24 +147,12 @@ function targetClick(value) {
 
   console.log("Throw: " + throwNum);
   console.log("Scores: " + pScores);
-  console.log("Bot Scores: " + bScores);
-}
-
-// Sets the bot up
-// with scoring probability
-function botSetup(black, red, blue, cCall, cHit) {
-  botWeights[0] = black; // Sets black
-  botWeights[1] = red; // Sets red
-  botWeights[2] = blue; // Sets blue
-  botWeights[3] = 100 - black - blue - red; // Sets miss
-  botClutch[0] = cCall; // Sets clutch call rate
-  botClutch[1] = cHit; // Sets clutch hit rate
 }
 
 // Bot takes turn
 function botTurn() {
   let ac = Math.random() * 100;
-  console.log("Rnd: " + ac);
+  console.log("========Rnd: " + ac);
 
   // If last throw and bot calls clutch
   if (throwNum == 5 && ac < botClutch[0]) {
@@ -145,7 +194,6 @@ function moveRound() {
 
       // TODO: Check win shit here
 
-
     } else targetClick('undo');
 
   }else if (confirm("Move to Round " + (round + 2) + "?")) {
@@ -170,4 +218,85 @@ function moveRound() {
   } else {
     targetClick('undo');
   }
+}
+
+// Used for choosing AI Opponent
+function chooseBot() {
+  // Creates a form for Ai
+  var form = document.createElement("form");
+  form.setAttribute("id","botStuff");
+
+  // Title
+  var tit = document.createElement("h2");
+  tit.innerHTML = "<b>Choose Bot</b>";
+
+  // Choose which bot to play
+  var ai = document.createElement("select");
+  ai.setAttribute('id', "ai");
+  // Spenny
+  var op = new Option();
+  op.value = "Spenny";
+  op.text = "Spenny";
+  ai.options.add(op);
+  // Marksman
+  var op = new Option();
+  op.value = "Marksman";
+  op.text = "Marksman";
+  ai.options.add(op);
+  // Rander
+  var op = new Option();
+  op.value = "Rander";
+  op.text = "Rander";
+  ai.options.add(op);
+  // Battle Axe
+  var op = new Option();
+  op.value = "Battle Axe";
+  op.text = "Battle Axe";
+  ai.options.add(op);
+  // Coltron
+  var op = new Option();
+  op.value = "Coltron";
+  op.text = "Coltron";
+  ai.options.add(op);
+  // Custom Option
+  /*var op = new Option();
+  op.value = "Custom";
+  op.text = "Custom(Currently Does Nothing)";
+  ai.options.add(op);*/
+  ai.value = "Rander"; // Default
+  var aiLab = document.createElement("Label");
+  aiLab.htmlFor = "text";
+  aiLab.innerHTML = "<b>Opponent:</b>";
+
+/*
+  // Play to win function
+  var cBox = document.createElement("input");
+  cBox.type = 'checkbox';
+  cBox.id = 'p2wBut';
+  cBox.name = 'p2wBut';
+  cBox.value = 'car?';
+  var cLab = document.createElement("label");
+  cLab.htmlFor = 'p2wBut';
+  cLab.innerHTML = "<br><b>Play To Win(Currently Doesn't Work):<b>";
+  */
+
+  // Submit button
+  var but = document.createElement("button");
+  but.innerHTML = "Submit";
+  but.type = "button";
+  but.onclick = function() {
+    botSetup(ai.value);
+  }
+
+  // <p> for styling
+  var para = document.createElement("p");
+
+  // Adds everything to the screen
+  form.appendChild(tit);
+  form.appendChild(aiLab);
+  form.appendChild(ai);
+  //form.appendChild(cLab);
+  //form.appendChild(cBox);
+  form.appendChild(but);
+  document.body.appendChild(form);
 }
